3e7bec0912b55312b21f63aed110a03731451151
Example User <nakazawatarou@gmail.com>
2025-03-24 10:40:03 +0900

    Fix index creation in migration

--- db/migrate/20250322060323_update_tags_table.rb
+++ db/migrate/20250322060323_update_tags_table.rb
@@ -3,7 +3,8 @@ class UpdateTagsTable < ActiveRecord::Migration[7.1]
     change_column_null :tags, :prompt_id, true
     
     # 一度インデックスを削除
-    remove_index :tags, [:prompt_id, :name]
+    remove_index :tags, [:prompt_id, :name], if_exists: true
+    remove_index :tags, :name, if_exists: true
     
     # インデックスを再作成
     add_index :tags, [:prompt_id, :name], unique: true, where: "prompt_id IS NOT NULL"
