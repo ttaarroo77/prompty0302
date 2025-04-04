e276bec2c0a5fcfc19b324824496e9360e4c5b64
Example User <nakazawatarou@gmail.com>
2025-04-05 03:39:45 +0900

    fix_test_add_taggings_association

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -2,6 +2,7 @@ class Prompt < ApplicationRecord
   belongs_to :user
   has_many :prompt_tags, dependent: :destroy
   has_many :tags, through: :prompt_tags
+  has_many :taggings, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 15 }
   validates :content, presence: true
