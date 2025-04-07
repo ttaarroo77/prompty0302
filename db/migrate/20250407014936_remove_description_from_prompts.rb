9cac6c315a7dd724adb75ff54f6630c765824d5d
Example User <nakazawatarou@gmail.com>
2025-04-07 11:42:13 +0900

    Remove duplicate migration files

--- db/migrate/20250407014936_remove_description_from_prompts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDescriptionFromPrompts < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :prompts, :description, :text
-  end
-end 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_05_113548) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_07_021003) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,7 +41,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_05_113548) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "description"
     t.string "url"
     t.index ["user_id"], name: "index_prompts_on_user_id"
   end
