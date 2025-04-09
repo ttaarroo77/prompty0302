de44ac89e640c7d1cfa6b1c3ce89f2bdb907703c
Example User <nakazawatarou@gmail.com>
2025-04-09 11:17:29 +0900

    Update SMTP settings to use SendGrid credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,4 +74,19 @@ Rails.application.configure do
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts << ENV.fetch("HOST", "prompty03.herokuapp.com")
   config.hosts << "prompty0302-492bf5fe391f.herokuapp.com"
+
+  # メール送信設定
+  config.action_mailer.default_url_options = { host: 'prompty0302-492bf5fe391f.herokuapp.com', protocol: 'https' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    domain: 'heroku.com',
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
