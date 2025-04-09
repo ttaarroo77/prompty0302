98a33ebfa36df7bf372d4f2f498263bad1d4875c
Example User <nakazawatarou@gmail.com>
2025-04-09 11:24:10 +0900

    Update SMTP settings to use Gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,11 +81,11 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-    address: 'smtp.sendgrid.net',
+    address: 'smtp.gmail.com',
     port: 587,
-    domain: 'heroku.com',
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'herokuapp.com',
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
