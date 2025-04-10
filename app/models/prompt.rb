a810296913704a879e53697d63eb5adb088d2bbc
Example User <nakazawatarou@gmail.com>
2025-04-10 12:36:55 +0900

    プロンプト削除時に関連するタグ提案も削除されるよう修正

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -2,6 +2,7 @@ class Prompt < ApplicationRecord
   belongs_to :user
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
+  has_many :tag_suggestions, class_name: 'AI::TagSuggestion', dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 15 }
   validates :notes, length: { maximum: 1000 }, allow_blank: true
